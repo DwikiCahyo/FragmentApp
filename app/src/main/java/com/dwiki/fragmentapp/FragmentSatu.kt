fc89bf92ef21174a0fba43e27bebc9fdf447f418
DwikiCahyo <70952085+DwikiCahyo@users.noreply.github.com>
2023-03-24 21:46:04 +0700

    Initial commit

---
+++ app/src/main/java/com/dwiki/fragmentapp/FragmentSatu.kt
@@ -0,0 +1,34 @@
+package com.dwiki.fragmentapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dwiki.fragmentapp.databinding.FragmentSatuBinding
+
+class FragmentSatu : Fragment() {
+
+    private var _binding:FragmentSatuBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSatuBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
